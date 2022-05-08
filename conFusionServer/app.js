a225f0e75520125d51086a0610e2dbf286f63cd5
xubailong <bailongctui@gmail.com>
2022-05-08 14:05:57 +0800

    Cors

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -94,6 +94,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
